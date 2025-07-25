5589fd6393ad0e8a7f94abe93ab6979a1b6df8d1
Anshggoel <Anshggoel112233@gmail.com>
2025-07-25 21:15:31 +0530

    Added Tower of Hanoi using Recursion

---
+++ recursion/9_tower_of_hanoi.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void hanoi(int n, char src, char dest, char helper) {
+    if (n == 0) return;
+    hanoi(n - 1, src, helper, dest);
+    cout << "Move disk " << n << " from " << src << " to " << dest << endl;
+    hanoi(n - 1, helper, dest, src);
+}
+
+int main() {
+    hanoi(3, 'A', 'C', 'B');
+    return 0;
+}
+    
