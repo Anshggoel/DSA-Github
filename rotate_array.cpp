5966b4f60f291582d0dbcae030379fdce61ce35b
Anshggoel <Anshggoel112233@gmail.com>
2025-07-25 21:04:20 +0530

    Added Rotate Array

---
+++ rotate_array.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void rotate(vector<int>& nums, int k) {
+    k = k % nums.size();
+    reverse(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k, nums.end());
+}
+
+int main() {
+    vector<int> nums = {1,2,3,4,5,6,7};
+    rotate(nums, 3);
+    for(int n : nums) cout << n << " ";
+}
