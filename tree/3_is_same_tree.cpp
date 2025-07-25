1e7821af355f23592fd69f6335b8c197723a8434
Anshggoel <Anshggoel112233@gmail.com>
2025-07-25 21:24:34 +0530

    Added Check if Two Binary Trees are Same

---
+++ tree/3_is_same_tree.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int x): val(x), left(NULL), right(NULL) {}
+};
+
+bool isSameTree(TreeNode* p, TreeNode* q) {
+    if (!p && !q) return true;
+    if (!p || !q || p->val != q->val) return false;
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
+
+int main() {
+    TreeNode* a = new TreeNode(1);
+    TreeNode* b = new TreeNode(1);
+    cout << (isSameTree(a, b) ? "Yes" : "No");
+}
