d927cbf925eff7f2690fadc383fa1e4bb8539105
Anshggoel <Anshggoel112233@gmail.com>
2025-07-25 20:59:58 +0530

    Added Move Zeroes

---
+++ move_zeroes.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+void moveZeroes(vector<int>& nums) {
+    int idx = 0;
+    for(int i = 0; i < nums.size(); i++) {
+        if(nums[i] != 0) nums[idx++] = nums[i];
+    }
+    while(idx < nums.size()) nums[idx++] = 0;
+}
+int main() {
+    vector<int> nums = {0,1,0,3,12};
+    moveZeroes(nums);
+    for(int x : nums) cout << x << " ";
+}
